8cf2d5bbf6625f4354d6087f5ce41bfe09260308
vanban22 <ban2206206@gmail.com>
2025-11-05 15:54:06 +0700

    be

--- lab/lab2.js
+++ lab/lab2.js
@@ -27,3 +27,46 @@ student.grade = "A";
 student.age = 21;
 
 console.log(student);
+
+//bai3 Tạo template cho email thông báo
+
+const user = {
+    firstName: "Nguyen",
+    lastName: "Van A",
+    product: "Laptop Dell XPS",
+    price: 25000000,
+    orderDate: "2024-01-15",
+};
+
+// Tạo template string cho email
+const emailTemplate = ` xin chao ${user.firstName} ${user.lastName} cam on ban da mua ${user.product}voi gia ${user.price} vao ngay ${user.orderDate}`;// Viết template ở đây 
+
+console.log(emailTemplate);
+
+//bai4 // Tạo HTML template
+
+const product = {
+    name: "iPhone 15",
+    price: 20000000,
+    discount: 10,
+    inStock: true,
+};
+
+// Tính giá sau giảm (code này đã có sẵn)
+const finalPrice = product.price * (1 - product.discount / 100);
+
+// Tạo template HTML
+// Mình dùng biểu thức 3 ngôi (ternary operator) để kiểm tra "inStock"
+const productCard = `
+  San pham${product.name}
+    Giá gốc: ${product.price.toLocaleString('vi-VN')} VND
+  
+    Giá khuyến mãi: ${finalPrice.toLocaleString('vi-VN')} VND (Giảm ${product.discount}%)
+  
+    Tình trạng: ${product.inStock ? 'Còn hàng' : 'Hết hàng'}
+`;
+console.log("\n--- TEMPLATE HTML ---");
+console.log(productCard);
+
+//bai 5
+
